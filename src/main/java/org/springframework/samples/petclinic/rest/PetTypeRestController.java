2a23fd31bddba4e8879cb6de6efecdfe6da14874
justyna <justyna950803@gmail.com>
2019-06-17 19:14:35 +0200

    issue 16

--- src/main/java/org/springframework/samples/petclinic/rest/PetTypeRestController.java
+++ src/main/java/org/springframework/samples/petclinic/rest/PetTypeRestController.java
@@ -28,7 +28,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.samples.petclinic.model.PetType;
-import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.samples.petclinic.service.PetTypeService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
@@ -72,27 +71,18 @@ public class PetTypeRestController {
     @PreAuthorize( "hasRole(@roles.VET_ADMIN)" )
 	@RequestMapping(value = "", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<PetType> addPetType(@RequestBody @Valid PetType petType, BindingResult bindingResult, UriComponentsBuilder ucBuilder){
-		BindingErrorsResponse errors = new BindingErrorsResponse();
-		HttpHeaders headers = new HttpHeaders();
-		if(bindingResult.hasErrors() || (petType == null)){
-			errors.addAllErrors(bindingResult);
-			headers.add("errors", errors.toJSON());
-			return new ResponseEntity<PetType>(headers, HttpStatus.BAD_REQUEST);
+        if(bindingResult.hasErrors() || (petType == null)){
+            return createPetTypeErrorResponses(bindingResult);
 		}
 		this.petTypeService.savePetType(petType);
-		headers.setLocation(ucBuilder.path("/api/pettypes/{id}").buildAndExpand(petType.getId()).toUri());
-		return new ResponseEntity<PetType>(petType, headers, HttpStatus.CREATED);
+		return new ResponseEntity<PetType>(petType, createLocationHeader(ucBuilder, petType), HttpStatus.CREATED);
 	}
 
     @PreAuthorize( "hasRole(@roles.VET_ADMIN)" )
 	@RequestMapping(value = "/{petTypeId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public ResponseEntity<PetType> updatePetType(@PathVariable("petTypeId") int petTypeId, @RequestBody @Valid PetType petType, BindingResult bindingResult){
-		BindingErrorsResponse errors = new BindingErrorsResponse();
-		HttpHeaders headers = new HttpHeaders();
 		if(bindingResult.hasErrors() || (petType == null)){
-			errors.addAllErrors(bindingResult);
-			headers.add("errors", errors.toJSON());
-			return new ResponseEntity<PetType>(headers, HttpStatus.BAD_REQUEST);
+            return createPetTypeErrorResponses(bindingResult);
 		}
 		PetType currentPetType = this.petTypeService.findPetTypeById(petTypeId);
 		if(currentPetType == null){
@@ -102,6 +92,20 @@ public class PetTypeRestController {
 		this.petTypeService.savePetType(currentPetType);
 		return new ResponseEntity<PetType>(currentPetType, HttpStatus.NO_CONTENT);
 	}
+
+    private HttpHeaders createLocationHeader(UriComponentsBuilder ucBuilder, PetType petType){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(ucBuilder.path("/api/pettypes/{id}").buildAndExpand(petType.getId()).toUri());
+        return headers;
+    }
+
+	private ResponseEntity<PetType> createPetTypeErrorResponses(BindingResult bindingResult){
+        BindingErrorsResponse errors = new BindingErrorsResponse();
+        HttpHeaders headers = new HttpHeaders();
+        errors.addAllErrors(bindingResult);
+        headers.add("errors", errors.toJSON());
+        return new ResponseEntity<PetType>(headers, HttpStatus.BAD_REQUEST);
+    }
 
     @PreAuthorize( "hasRole(@roles.VET_ADMIN)" )
 	@RequestMapping(value = "/{petTypeId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
